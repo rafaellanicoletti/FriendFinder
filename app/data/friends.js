60ab91f73a9e8192fac00d13f762b9fff702edd7
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-23 10:18:42 -0400

    firneds js

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+        name: "John",
+        photo: "https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/best-profile-pic13-300x300.jpg",
+        scores: [5, 1, 4, 3, 1, 2, 5, 3, 3, 1]
+    },
+    {
+        name: "Mary",
+        photo: "http://www.sarkarinaukrisearch.in/wp-content/uploads/2017/02/flower-profile-images.jpg",
+        scores: [2, 5, 4, 1, 2, 4, 3, 2, 3, 3]
+    },
+];
+
+module.exports = friends;
