499975a5ddce991dafd6c1426a423d56920b8016
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-23 18:00:54 -0400

    added friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,17 @@ var friends = [
     {
         name: "John",
         photo: "https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/best-profile-pic13-300x300.jpg",
-        scores: [5, 1, 4, 3, 1, 2, 5, 3, 3, 1]
+        scores: [5, 1, 4, 3, 1, 2, 5, 4, 2, 1]
     },
     {
         name: "Mary",
         photo: "http://www.sarkarinaukrisearch.in/wp-content/uploads/2017/02/flower-profile-images.jpg",
-        scores: [2, 5, 4, 1, 2, 4, 3, 2, 3, 3]
+        scores: [2, 5, 4, 3, 2, 4, 1, 2, 5, 3]
+    },
+    {
+        name: "Melanie",
+        photo: "https://www.girlschase.com/sites/default/files/styles/770x415/public/female-mating-motivations-preview.jpg?itok=ZC2RYCPl",
+        scores: [1, 2, 3, 5, 5, 4, 3, 1, 3, 3]
     },
 ];
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
 
     });
-
+    
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
     
 }
 
