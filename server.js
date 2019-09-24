379914e8f03419867ab5a92ac0b1e83a33b28224
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-24 12:42:31 -0400

    server didnt work, now it does

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-// Dependencies
+// // Dependencies
 
-var express = require("express");
-var path = require("path");
+// var express = require("express");
+// var path = require("path");
+
+// var app = express();
+// var PORT = process.env.PORT || 8000
+
+
+// app.listen(PORT, function(){
+//     console.log("Friend Finder is listening on port: " + PORT);
+// });
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 
-app.listen(PORT, function(){
-    console.log("Friend Finder is listening on port: " + PORT);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
